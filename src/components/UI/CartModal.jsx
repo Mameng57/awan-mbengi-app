2b50d78d0e9dc4fd9db8b39c0b5e5605ba76be61
Mameng57 <takiya2888157@gmail.com>
2022-01-22 10:11:18 +0700

    refactor code, to acomodates changes to CartModal

--- src/components/UI/CartModal.jsx
+++ src/components/UI/CartModal.jsx
@@ -1,21 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import Styles from "./Styles/CartModal.module.css";
-import ListItem from "./ListItem";
+import { motion } from "framer-motion";
 import { useReactToPrint } from "react-to-print";
-
-const Backdrop = ({children, setModal}) => {
-
-  return (
-    <motion.div className={Styles.backdrop}
-      onClick={() => setModal(false)}
-      initial={{opacity: 0}}
-      animate={{opacity: 1}}
-      exit={{opacity: 0}}>
-      {children}
-    </motion.div>
-  );
-}
+import Backdrop from "./Backdrop";
+import Styles from "./Styles/CartModal.module.css";
+import CartTable from "./CartTable";
 
 const CartModal = ({setModal, items, subCounter, deleteItem}) => {
 
@@ -26,134 +14,64 @@ const CartModal = ({setModal, items, subCounter, deleteItem}) => {
 
   useEffect(
     () => {
-      setTotalPrice(
-        () => {
-          let amount = 0;
+      let amount = 0;
 
-          cartItems.forEach(
-            (element) => {
-              amount += element.total;
-            }
-          );
-
-          return amount;
-        }
-      );
+      cartItems.forEach((element) => amount += element.total);
+      setTotalPrice(amount);
     }, [totalPrice, cartItems]
   );
-
-  const updatePrice = (idx) => {
-    setCartItems(
-      (prevState) => {
-        const array = [...prevState];
-        array[idx]['total'] = array[idx]['price'] * array[idx]['quantity'];
-
-        return array;
-      }
-    );
-  }
 
   const validateNumber = (value) => {
     const regex = /^[0-9]+$/;
     return regex.test(value);
   }
 
-  const addQuantity = (idx) => {
+  const updatePrice = (idx) => {
     setCartItems(
       (prevState) => {
-        const array = [...prevState];
-        array[idx]['quantity'] += 1;
-
-        return array;
+        prevState[idx]['total'] = prevState[idx]['price'] * prevState[idx]['quantity'];
+        return [...prevState];
       }
     );
-    updatePrice(idx);
   }
 
-  const subQuantity = (idx) => {
-    if (!validateNumber(cartItems[idx]['quantity'] - 2)) return;
+  const changeQtyHandler = (op, idx, event) => {
+    if (op === "add") {
+      setCartItems(
+        (prevState) => {
+          prevState[idx]['quantity'] += 1;
+          return [...prevState];
+        }
+      );
+    }
+    else if (op === "sub") {
+      if (!validateNumber(cartItems[idx]['quantity'] - 2)) return;
 
-    setCartItems(
-      (prevState) => {
-        const array = [...prevState];
-        array[idx]['quantity'] -= 1;
+      setCartItems(
+        (prevState) => {
+          prevState[idx]['quantity'] -= 1;
+          return [...prevState];
+        }
+      );
+    }
+    else {
+      if (!event.target.value) return;
+      if (!validateNumber(event.target.value)) return;
 
-        return array;
-      }
-    );
-    updatePrice(idx);
-  }
-
-  const changeQuantityHandler = (event, idx) => {
-    if (!event.target.value) return;
-    if (!validateNumber(event.target.value)) return;
-
-    setCartItems(
-      (prevState) => {
-        const array = [...prevState];
-        array[idx]['quantity'] = parseInt(event.target.value);
-
-        return array;
-      }
-    );
+      setCartItems(
+        (prevState) => {
+          prevState[idx]['quantity'] = parseInt(event.target.value);
+          return [...prevState];
+        }
+      );
+    }
     updatePrice(idx);
   }
 
   const deleteHandler = (id) => {
-    setCartItems([...cartItems.filter((value) => value.id !== id)])
+    setCartItems([...cartItems.filter((value) => value.id !== id)]);
     deleteItem(id);
     subCounter();
-  }
-
-  const listIsEmpty = () => {
-    if (cartItems.length === 0) {
-      return (
-        <div className={Styles.empty}>
-          <div className={Styles['awan-mbengi']} />
-          <h4>Silahkan pilih menu, untuk ditambahkan ke Keranjang...</h4>
-        </div>
-      );
-    }
-
-    return (
-      <div className={Styles.container} ref={componentRef}>
-        <div className={Styles.header}>
-          <div className={Styles.header__column}><h3>Menu</h3></div>
-          <div className={Styles.header__column}><h3>Jumlah</h3></div>
-          <div className={Styles.header__column}><h3>Harga</h3></div>
-          <div className={Styles.header__column}><h3>Hapus</h3></div>
-        </div>
-        <div className={Styles.items}> 
-        <AnimatePresence initial={false}> {
-          cartItems.map(
-            (item, idx) => (
-              <ListItem key={item.id}
-                item={item} idx={idx} cartItems={cartItems}
-                changeQuantity={changeQuantityHandler}
-                addQuantity={addQuantity} subQuantity={subQuantity} deleteHandler={deleteHandler}
-              />
-            )
-          )
-        }
-        </AnimatePresence>
-        </div>
-        <div className={Styles.footer}>
-          <div className={Styles.total}>
-            <span>TOTAL</span>
-            <h3>{totalPrice}</h3>
-          </div>
-          <div className={Styles.order}>
-            <div className={Styles.reservation} onClick={printHandler}>
-              Reservasi
-            </div>
-            <div className={Styles.checkout}>
-              Checkout
-            </div>
-          </div>
-         
-        </div>
-      </div>
-    );
   }
 
   return (
@@ -163,7 +81,13 @@ const CartModal = ({setModal, items, subCounter, deleteItem}) => {
         onClick={(event) => event.stopPropagation()}
         initial={{y: "-100vh"}} animate={{y: 0}} exit={{y: "100vh"}}
         transition={{type: "just", damping: 15}}>
-        { listIsEmpty() }
+        <CartTable ref={componentRef}
+          cartItems={cartItems}
+          changeQtyHandler={changeQtyHandler}
+          deleteHandler={deleteHandler}
+          printHandler={printHandler}
+          totalPrice={totalPrice}
+        />
       </motion.div>
     </Backdrop>
   );
--- src/components/UI/ListItem.jsx
+++ src/components/UI/ListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Styles from "./Styles/ListItem.module.css";
 
-const ListItem = ({item, idx, changeQuantity, addQuantity, subQuantity, deleteHandler}) => {
+const ListItem = ({item, idx, changeQty, deleteHandler}) => {
 
   return (
     <motion.div
@@ -17,22 +17,22 @@ const ListItem = ({item, idx, changeQuantity, addQuantity, subQuantity, deleteHa
         </div>
       </div>
       <div className={Styles.header__column}>
-      <svg xmlns="http://www.w3.org/2000/svg"
-        className={Styles.button}
-        fill="none" viewBox="0 0 24 24"
-        stroke="currentColor" onClick={() => subQuantity(idx)}>
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 12H4" />
-      </svg>
-      <div className={Styles.input}>
-        <input type="number" min="1" value={item.quantity} onChange={(event) => changeQuantity(event, idx)}
-        />
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg"
-        className={Styles.button}
-        fill="none" viewBox="0 0 24 24"
-        stroke="currentColor" onClick={() => addQuantity(idx)}>
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
-      </svg>
+        <svg xmlns="http://www.w3.org/2000/svg"
+          className={Styles.button}
+          fill="none" viewBox="0 0 24 24"
+          stroke="currentColor" onClick={() => changeQty("sub", idx)}>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 12H4" />
+        </svg>
+        <div className={Styles.input}>
+          <input type="number" min="1" value={item.quantity} onChange={(event) => changeQty("", idx, event)}
+          />
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg"
+          className={Styles.button}
+          fill="none" viewBox="0 0 24 24"
+          stroke="currentColor" onClick={(event) => changeQty("add", idx, event)}>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
+        </svg>
       </div>
       <div className={Styles.header__column}>
         <h3>{item['total']}</h3>
